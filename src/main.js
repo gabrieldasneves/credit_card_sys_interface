8a6f798c9621c3a4a888a64f62ca89c80d8735ed
Gabriel das Neves <galvesdasneves@gmail.com>
2022-10-18 14:25:05 -0300

    first step with DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./css/index.css"
+
+const CCBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const CCBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector("cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436d99", "32d57f2"],
+    mastercard: ["#df6f29", "#c69347"],
+    default: ["black", "gray"],
+  }
+
+  CCBgColor01.setAttribute("fill", colors[type][0])
+  CCBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCardType("default")
+
+globalThis.setCardType = setCardType
