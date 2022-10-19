5db1cda4ba8b81630f73467bbdf1a6aff8be33c3
Gabriel das Neves <galvesdasneves@gmail.com>
2022-10-19 16:40:29 -0300

    working with IMask

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
 import "./css/index.css"
+import IMask from "imask"
 
-const CCBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-const CCBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-const ccLogo = document.querySelector("cc-logo span:nth-child(2) img")
-
-function setCardType(type) {
-  const colors = {
-    visa: ["#436d99", "32d57f2"],
-    mastercard: ["#df6f29", "#c69347"],
-    default: ["black", "gray"],
-  }
-
-  CCBgColor01.setAttribute("fill", colors[type][0])
-  CCBgColor02.setAttribute("fill", colors[type][1])
-  ccLogo.setAttribute("src", `cc-${type}.svg`)
-}
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 setCardType("default")
 
+function setCardType(typeCard) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[typeCard][0])
+  ccBgColor02.setAttribute("fill", colors[typeCard][1])
+  ccLogo.setAttribute("src", `/cc-${typeCard}.svg`)
+}
+
 globalThis.setCardType = setCardType
+
+const securityCode = document.getElementById("security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2) + 0,
+      to: String(new Date().getFullYear() + 10).slice(2) + 0,
+    },
+
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
